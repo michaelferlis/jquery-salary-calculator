5c9cd47820ade5a76a226876569720a3325e2cd4
Michael Ferlis <michaelferlis@gmail.com>
2019-05-17 16:11:06 -0500

    add agent working

--- client.js
+++ client.js
@@ -8,10 +8,31 @@ function onReady(){
 }
 
 function addAgent(){
+    console.log('add agent working');
+    
+    
     let firstName = $("#firstName").val();
     let lastName = $("#lastName").val();
     let identification = $("#identification").val();
     let title = $("#title").val();
     let salary = $("#annualSalary").val();
     
-}
+
+    $("#agentTableBody").append (
+
+        `<tr>
+        
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${identification}</td>
+        <td>${title}</td>
+        <td>${salary}</td>
+        
+        </tr>`
+        
+        
+        ) // end agent table body append
+
+
+} //
+
